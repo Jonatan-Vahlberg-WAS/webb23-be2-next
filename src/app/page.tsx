932a04c8d49570c61e5142a1d881b1fc37eb974f
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-10-03 09:08:08 +0200

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
-import Assert from "@/components/Assert";
-import Generics from "@/components/Generics";
-import Generics2 from "@/components/Generics2";
-import Generics3Server from "@/components/Generics2server";
-import { NextPage } from "next";
+
 
 export default function Home({searchParams}: any) {
   console.clear();
@@ -10,12 +6,6 @@ export default function Home({searchParams}: any) {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Assert/>
-        {/* <Generics/> */}
-        {/* <Generics2/> */}
-        <Generics3Server
-          search={searchParams.search}
-        />
        </main>
     </div>
   );
