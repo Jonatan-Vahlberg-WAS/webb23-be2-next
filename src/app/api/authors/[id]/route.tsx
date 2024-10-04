59246926f3ce4b66adffc413b1af09eba50d72da
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-10-04 09:08:45 +0200

    add task starter code

---
+++ src/app/api/authors/[id]/route.tsx
@@ -0,0 +1,8 @@
+
+
+//TODO: get 1 author on "id"
+
+//TODO: update author GET + POST
+//TODO: validate incoming data
+
+//TODO: delete author on "id"
---
+++ src/app/api/authors/route.tsx
@@ -0,0 +1,7 @@
+
+
+//TODO: Get all authors
+//TODO extra: filter authors on first_name
+
+//TODO: Create author
+//TODO: Validate incoming data
