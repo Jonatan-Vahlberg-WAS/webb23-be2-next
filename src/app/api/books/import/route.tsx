b3188156cc9f1d1a2a1f4db1efca6c58459b0766
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-10-10 11:20:18 +0200

    add book importing function

---
+++ src/app/api/books/import/route.tsx
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function POST(request: NextRequest) {
+  try {
+    const books: BookData[] = await request.json();
+    const storedBooks = await prisma.book.findMany();
+    const distinctBooks = books.filter((a1) => {
+      return storedBooks.every(
+        (a2) => a1.title !== a2.title && a1.authorId !== a2.authorId
+      );
+    });
+    if (distinctBooks.length === 0) {
+      return NextResponse.json({
+        count: 0,
+      });
+    }
+    const response = await prisma.book.createMany({
+      data: distinctBooks.map((book) => ({
+        title: book.title,
+        authorId: book.authorId,
+      })),
+    });
+
+    return NextResponse.json(response);
+  } catch (error: any) {
+    console.warn("Error importing books", error);
+    return NextResponse.json(
+      {
+        message: "Error importing books",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+}
