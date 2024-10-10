1d0a4348bd6a6692fc0933865fcb74c3cc16d76c
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-10-10 10:29:41 +0200

    add login starter code

---
+++ src/app/api/auth/login/route.tsx
@@ -0,0 +1,16 @@
+import { NextRequest } from "next/server";
+
+
+export async function POST(request: NextRequest) {
+    //TODO: Implement user login
+
+    //TODO: validate incoming data
+
+    //TODO: find user on email
+    //!ELSE: return 400 "user matching credentials not found"
+
+    //TODO: validate password is matching
+    //!ELSE: return 400 "user matching credentials not found"
+
+    //TODO: return user
+}
