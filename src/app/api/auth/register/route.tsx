0a2fcd4c17f4cde46e163f107396357ec2530eac
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2024-10-10 10:27:10 +0200

    add user validation

--- src/app/api/auth/register/route.tsx
+++ src/app/api/auth/register/route.tsx
@@ -1,31 +1,53 @@
 import { UserRegistrationData } from "@/types/user";
+import { userExists } from "@/utils/prisma";
+import { userRegistrationValidator } from "@/utils/validators/userValidator";
 import { PrismaClient, User } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
-
 const prisma = new PrismaClient();
 
-
-export async function POST(request:NextRequest) {
-    try {
-        const body: UserRegistrationData = await request.json()
-        //TODO validate fields
-        const user: User = await prisma.user.create({
-            data: {
-                email: body.email,
-                password: body.password,
-                name: body.name
-            }
-        })
-        return NextResponse.json(user, { status: 201 })
-    } catch (error: any) {
-        return NextResponse.json(
-            {
-              message: "A valid 'user registration' object has to be sent",
-            },
-            {
-              status: 400,
-            }
-          );
+export async function POST(request: NextRequest) {
+  try {
+    const body: UserRegistrationData = await request.json();
+    const [hasErrors, errors] = userRegistrationValidator(body);
+    if (hasErrors) {
+      return NextResponse.json(
+        {
+          errors,
+        },
+        {
+          status: 400,
+        }
+      );
     }
+
+    const exists = await userExists(body.email, prisma);
+    if (exists) {
+      return NextResponse.json(
+        {
+          message: "User with this email already exists",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+    const user: User = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+        name: body.name,
+      },
+    });
+    return NextResponse.json(user, { status: 201 });
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        message: "A valid 'user registration' object has to be sent",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
 }
---
+++ src/utils/validators/userValidator.ts
@@ -0,0 +1,19 @@
+import { UserRegistrationData } from "@/types/user";
+
+export function userRegistrationValidator(data: UserRegistrationData): [boolean, ErrorObject] {
+    let errors: ErrorObject = {};
+    if (!data.email) {
+      errors.title = "Email is required";
+      //TODO: Add email validation
+    }
+    if (!data.password) {
+      errors.author = "Password is required";
+    }
+    if(!data.name) {
+        errors.name = "Name is required";
+    }
+    const hasErrors = Object.keys(errors).length !== 0;
+  
+    return [hasErrors, errors]
+  }
+  
